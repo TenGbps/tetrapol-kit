ac772e95eb40ebd182eff2a243699e469b0c3718
Jiri Svoboda <jiri.svoboda@yandex.com>
2015-08-03 16:21:29 +0200

    misc: add SIZEOF for struct members

--- lib/tetrapol/misc.h
+++ lib/tetrapol/misc.h
@@ -3,6 +3,7 @@
 #include <stdint.h>
 
 #define ARRAY_LEN(a) (sizeof(a) / sizeof(a[0]))
+#define SIZEOF(s, i) (sizeof(((s*)(NULL))->i))
 
 // TODO: transfer to log.h
 void print_hex(const uint8_t *bytes, int n);
