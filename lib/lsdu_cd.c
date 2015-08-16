0e7f61bf7d04bf30785e7bc7bfd5a1f3d0984f91
SysOp <sysop@centrum.cz>
2015-08-17 01:00:15 +0200

    LPDU: lsdu layer

---
+++ lib/lsdu_cd.c
@@ -0,0 +1,124 @@
+#define LOG_PREFIX "lsdu_cd"
+#include <tetrapol/log.h>
+#include <tetrapol/lsdu_cd.h>
+#include <tetrapol/misc.h>
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+static const char *codop_str[256] = {
+    "N/A",                          // 0x00,
+    "TP_ADDRESS",                   // 0x01,
+    "TP_ALIAS",                     // 0x02,
+    "AMBIENCE_LISTENING",           // 0x03,
+    "TP_ADDRESS_CHIF",              // 0x04,
+    "TP_ADDRES_INHIB",              // 0x05,
+    "TP_ADDRESS_CHIF_INHIB",        // 0x06,
+    "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+    "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+    "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+    "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+    "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+    "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+    "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+    "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+    "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+    "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+    "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+    "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+    "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+    "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+    "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+    "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+    "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+    "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+    "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+    "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+    "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+    "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+    "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+    "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+    "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+    "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+    "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A", "N/A"
+    "N/A", "N/A", "N/A", "N/A", "N/A", "N/A",
+};
+
+void lsdu_cd_destroy(lsdu_cd_t *lsdu)
+{
+    free(lsdu);
+}
+
+static int tp_address_check(const uint8_t *data, int len)
+{
+    if (len < sizeof(lsdu_cd_tp_address_t)) {
+        LOG(WTF, "invalid lenght %d", len);
+        return -1;
+    }
+
+    if (len > sizeof(lsdu_cd_tp_address_t)) {
+        LOG(WTF, "unused bytes (%d)", len);
+    }
+
+    return 0;
+}
+
+int lsdu_cd_decode(const uint8_t *data, int len, lsdu_cd_t **lsdu)
+{
+    if (!lsdu) {
+        LOG(ERR, "lsdu == NULL");
+        return -1;
+    }
+
+    *lsdu = malloc(sizeof(lsdu_cd_t));
+    if (!*lsdu) {
+        return -1;
+    }
+
+    memcpy(*lsdu, data, len);
+    (*lsdu)->unknown.len = len;
+
+    switch ((*lsdu)->unknown.codop) {
+        case TP_ADDRESS:
+            return tp_address_check(data, len);
+
+        default:
+            LOG(INFO, "TODO LSDU_CD 0x%02x", (*lsdu)->unknown.codop);
+            break;
+    }
+
+    return 0;
+}
+
+static void tp_address_print(const lsdu_cd_tp_address_t *lsdu)
+{
+    char buf[3 * SIZEOF(lsdu_cd_tp_address_t, rt_address)];
+    LOGF("\tMODIFIER_NUMBER=%d\n", lsdu->modifier_number);
+    LOGF("\tSIGNATURE=%d\n", lsdu->signature);
+    LOGF("\tRT_ADDRESS=%s\n",
+            sprint_hex(buf, lsdu->rt_address,
+                SIZEOF(lsdu_cd_tp_address_t, rt_address)));
+}
+
+static void lsdu_cd_unknown_print(const lsdu_cd_t *lsdu)
+{
+    char buf[3 * sizeof(lsdu_cd_t)];
+    LOGF("\tdata=%s\n",
+            sprint_hex(buf, lsdu->unknown.data, lsdu->unknown.len));
+}
+
+void lsdu_cd_print(const lsdu_cd_t *lsdu)
+{
+    LOG_("CODOP=%d (%s)\n", lsdu->unknown.codop, codop_str[lsdu->unknown.codop]);
+
+    switch (lsdu->unknown.codop) {
+        case TP_ADDRESS:
+            tp_address_print(&lsdu->tp_address);
+            break;
+
+        default:
+            lsdu_cd_unknown_print(lsdu);
+    }
+}
+
---
+++ lib/tetrapol/lsdu_cd.h
@@ -0,0 +1,90 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+/**
+  PAS 0001-3-4 6.4.3
+  X_UNKNOWN_xx messages float in air but are not part of public specification
+  */
+enum {
+    // 0x00 reserved
+    TP_ADDRESS                  = 0x01,
+    TP_ALIAS                    = 0x02,
+    AMBIENCE_LISTENING          = 0x03,
+    TP_ADDRESS_CHIF             = 0x04,
+    TP_ADDRES_INHIB             = 0x05,
+    TP_ADDRESS_CHIF_INHIB       = 0x06,
+};
+
+typedef uint8_t codop_t;
+
+/// PAS 0001-3-4 6.4.2.1
+typedef struct {
+    uint8_t codop;
+    uint8_t signature : 4;
+    uint8_t modifier_number : 4;
+    uint8_t rt_address[9];
+} lsdu_cd_tp_address_t;
+
+typedef lsdu_cd_tp_address_t lsdu_cd_tp_addres_inhib_t;
+
+/// PAS 0001-3-4 6.4.2.2
+typedef struct {
+    uint8_t codop;
+    uint8_t signature : 4;
+    uint8_t modifier_number : 4;
+    uint8_t rt_alias[9];
+} lsdu_cd_tp_alias_t;
+
+/// PAS 0001-3-4 6.4.2.3
+typedef struct {
+    uint8_t codop;
+    uint8_t transmit_priority : 4;
+    uint8_t listen_mode : 4;
+    uint8_t called_addr[5];
+} lsdu_cd_ambience_listening_t;
+
+/*
+// ** Those are not defined in accessible PAS
+
+/// PAS 0001-3-4 6.4.2.4
+typedef struct {
+    uint8_t codop;
+} lsdu_cd_tp_address_chif_t;
+
+/// PAS 0001-3-4 6.4.2.6
+typedef struct {
+    uint8_t codop;
+} lsdu_cd_tp_address_chif_inhib_t;
+*/
+
+typedef struct {
+    codop_t codop;
+    // PAS 0001-3-4 6.2 lengh of LSDU is limited to 59 bytes (-1 for codop)
+    uint8_t data[58];
+    uint8_t len;
+} lsdu_cd_unknown_t;
+
+// do not use directly, this struct must be first member of each LSDU structure
+typedef union {
+    lsdu_cd_unknown_t unknown;
+    lsdu_cd_tp_address_t tp_address;
+    lsdu_cd_tp_addres_inhib_t tp_addres_inhib;
+    lsdu_cd_tp_alias_t tp_alias;
+    lsdu_cd_ambience_listening_t ambience_listening;
+    //lsdu_cd_tp_address_chif_t tp_address_chif;
+    //lsdu_cd_tp_address_chif_inhib_t tp_address_chif_inhib;
+} lsdu_cd_t;
+
+/**
+ * @brief lsdu_d_decode Compose LSDU from TPDUs.
+ * @param data LSDU data.
+ * @param len length of data in bytes
+ * @param lsdu Allocated lsdu_cd_t where result is stored.
+ * @return 0 on success, -1 on fail.
+ */
+int lsdu_cd_decode(const uint8_t *data, int len, lsdu_cd_t **lsdu);
+
+void lsdu_cd_print(const lsdu_cd_t *lsdu);
+void lsdu_cd_destroy(lsdu_cd_t *lsdu);
