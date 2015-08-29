490d48d98f9d05b7cd4135e58109a812ea757a65
Jiri Svoboda <jiri.svoboda@yandex.com>
2015-08-29 22:08:58 +0200

    tch: split SCH and VCH

--- lib/tch.c
+++ lib/tch.c
@@ -6,7 +6,7 @@
 
 struct tch_priv_t {
     sdch_t *sch;
-    // sdch_t *vch;
+    sdch_t *vch;
     // sch_ti;
 };
 
@@ -23,6 +23,13 @@ tch_t *tch_create(void)
         return NULL;
     }
 
+    tch->vch = sdch_create();
+    if (!tch->vch) {
+        sdch_destroy(tch->sch);
+        free(tch);
+        return NULL;
+    }
+
     return tch;
 }
 
@@ -30,6 +37,7 @@ void tch_destroy(tch_t *tch)
 {
     if (tch) {
         sdch_destroy(tch->sch);
+        sdch_destroy(tch->vch);
     }
 
     free(tch);
@@ -42,19 +50,13 @@ int tch_push_data_block(tch_t *tch, data_block_t *data_blk)
         return -1;
     }
 
-    // TODO: separate VCH, SCH, SCH_TI
-
-    if (data_blk->nerrs == 0 && (data_blk->fr_type == FRAME_TYPE_VOICE)) {
-        LOG(INFO,"VOICE FRAME asb=%i", data_block_get_asb(data_blk).xy);
-
-        return 0;
-    }
-
-    else if (data_blk->nerrs == 0 && (data_blk->fr_type == FRAME_TYPE_DATA)) {
-        LOG(INFO,"DATA FRAME asb=%i", data_block_get_asb(data_blk).xy);
-
-        if (sdch_dl_push_data_frame(tch->sch, data_blk)) {
-            tsdu_t *tsdu = sdch_get_tsdu(tch->sch);
+    if (data_block_get_asb(data_blk).x) {
+        if (data_blk->fr_type == FRAME_TYPE_VOICE) {
+            LOG(INFO,"VOICE FRAME asb=%i", data_block_get_asb(data_blk).xy);
+            return 0;
+        }
+        if (sdch_dl_push_data_frame(tch->vch, data_blk)) {
+            tsdu_t *tsdu = sdch_get_tsdu(tch->vch);
             if (tsdu) {
                 LOG_IF(INFO) {
                     LOG_("\n");
@@ -62,12 +64,31 @@ int tch_push_data_block(tch_t *tch, data_block_t *data_blk)
                 }
             }
             tsdu_destroy(tsdu);
-
-            return 0;
         }
+        return 0;
     }
 
-    return -1;
+    // TODO: separate SCH and SCH_TI
+
+    if (data_blk->fr_type != FRAME_TYPE_DATA) {
+        LOG(WTF, "data block expected");
+        return -1;
+    }
+
+    LOG(INFO,"DATA FRAME asb=%i", data_block_get_asb(data_blk).xy);
+
+    if (sdch_dl_push_data_frame(tch->sch, data_blk)) {
+        tsdu_t *tsdu = sdch_get_tsdu(tch->sch);
+        if (tsdu) {
+            LOG_IF(INFO) {
+                LOG_("\n");
+                tsdu_print(tsdu);
+            }
+        }
+        tsdu_destroy(tsdu);
+    }
+
+    return 0;
 }
 
 void tch_tick(const timeval_t *tv, void *tch_)
