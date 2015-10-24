1638c9a486d34b6c9682ae371acf1757bbd422bf
Jiri Svoboda <jiri.svoboda@yandex.com>
2015-10-24 02:05:56 +0200

    tsdu_json: interface for JSON output

---
+++ lib/tsdu_json.c
@@ -0,0 +1,18 @@
+#define LOG_PREFIX "tsdu_json"
+
+#include <tetrapol/log.h>
+#include <tetrapol/misc.h>
+#include <tetrapol/tsdu_json.h>
+
+
+void tsdu_json(const tpol_t *tpol, const tpol_tsdu_t *tsdu)
+{
+    printf("{ \"event\": \"tsdu\", ");
+
+    printf("\"tsdu\": { ");
+    {
+    }
+    printf("} ");
+
+    printf("}\n");
+}
